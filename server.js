6af0e312836d5229cf90d41a5199895d57b126b3
Sandeep Reddy <msande1996@gmail.com>
2017-01-17 19:47:00 +0530

    Add dependencies, Construct base api
    Refer to helper.txt for data format

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-var express=require()
+//requirements
+const express=require('express');
+const mongoClient=require('mongodb').MongoClient;
+
+const app=express();
+
+mongoClient.connect("mongodb://sande96:queenrocks96@ds011228.mlab.com:11228/dear_stranger",(err,database)=>{
+	if(err) return console.log(err);
+	db=database;
+	app.listen(3000,()=>{
+		console.log("Database connected and is up and running!");
+	});
+});
+
+//Send a greeting when root address is called
+app.get('/',rootCall);
+
+
+function rootCall(req,res) {
+	res.send("Welcome to DearStranger.");
+}
